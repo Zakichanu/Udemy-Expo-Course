83371380f2478c1fd980f98b3f86157c8ee6a16d
Zakaria Tobbal <tzakaria@hotmail.fr>
2024-03-13 23:51:17 +0100

    Introduction, useState and button

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [user, setUser] = useState('Zakaria TOBBAL');
+  const [fruit, setFruit] = useState({name: 'Apple', price: 5});
+  const changeName = () => {
+    setUser('LeBron James');
+    setFruit({name: 'Banana', price: 10});
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.myStyle}>{user} is eating {fruit.name} which costs {fruit.price} </Text>
+      <View style={styles.buttonStyle}><Button title="Change" onPress={changeName}/></View>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +27,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  myStyle: {
+    color: 'red',
+    fontSize: 20,
+    fontWeight: 'bold',
+    backgroundColor: 'yellow',
+  },
+  buttonStyle: {
+    width: 100,
+    height: 100,
+    marginTop: 20,
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# UDEMY COURSE OF MR Kim Chen
+
+[The link of the course](https://www.udemy.com/course/react-native-expo-for-multiplatform-ios-android-mobile-app-development/?couponCode=KEEPLEARNING)
